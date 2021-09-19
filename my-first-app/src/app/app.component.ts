b9f6167028af2c049aa8cbc40626c59a3e0b68dc
Breanna Hansen <hansbrea@hotmail.com>
2021-09-18 23:05:49 -0600

    Finished The Basics section

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
   styles: [`
   h3 {
     color: dodgerblue;
-  }`]
+  }
+  .white-text {
+    color: white;
+}`]
 })
 export class AppComponent {
+  username = '';
+  log = [];
+  showSecret = false;
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+
+  onLogClick() {
+    let count = this.log.length;
+    this.log.push(count + 1);
+  }
+
 }
