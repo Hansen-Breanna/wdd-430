41222afa3e737376844f3fca291c098acadfca2c
Breanna Hansen <hansbrea@hotmail.com>
2021-09-22 17:58:29 -0600

    Finished Course Project - the Basics

--- recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe-book.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
   ];
 
   constructor() { }
