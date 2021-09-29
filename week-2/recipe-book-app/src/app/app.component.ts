b0febf9faa199d0a55043e12a6f8c1cf5d076544
Breanna Hansen <hansbrea@hotmail.com>
2021-09-28 22:52:46 -0600

    Course Project-Components & Databinding navigation

--- week-2/recipe-book-app/src/app/app.component.ts
+++ week-2/recipe-book-app/src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+  
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
