c057a88dbede954d63d0d006064e32f828caef60
Breanna Hansen <hansbrea@hotmail.com>
2021-09-28 23:16:37 -0600

    Recipe Book passing data

--- week-2/recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ week-2/recipe-book-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe-book.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe-book.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg'),
+    new Recipe('Another Test Recipe', 'This is a second test', 'https://cdn.pixabay.com/photo/2017/12/10/14/47/pizza-3010062_960_720.jpg'),
   ];
 
   constructor() { }
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
