ccae2e2e7468642ef9fb5eca4c7979d811684d01
Breanna Hansen <hansbrea@hotmail.com>
2021-09-28 22:38:18 -0600

    Components & Databinding Deep Dive

---
+++ week-3/cmp-databinding/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onIntervalFired(firedNumber: number) {
+    if(firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+}
