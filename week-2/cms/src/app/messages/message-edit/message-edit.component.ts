c4ef011a4a64f61f5ef9b37f007f2a4adf0e95cf
Breanna Hansen <hansbrea@hotmail.com>
2021-09-30 17:56:00 -0600

    Finish lesson with errors

--- week-2/cms/src/app/messages/message-edit/message-edit.component.ts
+++ week-2/cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject', {static: true}) subjectInputRef: ElementRef;
+  @ViewChild('msgText', {static: true}) msgTextInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender = "Breanna Hansen";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSendMessage(msgTextInputRef: HTMLInputElement, subjectInputRef: HTMLInputElement) {
+    const msgSubject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgTextInputRef.nativeElement.value;
+    const newMessage = new Message(10, msgSubject, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+    this.subjectInputRef.nativeElement.value = "new";
+  }
+
+  onClear(msgTextInputRef: HTMLInputElement, subjectInputRef: HTMLInputElement) {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
 }
--- week-2/cms/src/app/messages/message-list/message-list.component.ts
+++ week-2/cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Array<Message> = [
+    new Message(1, "Welcome to John", "Welcome to our newest team member, John Pemberly!", "Breanna Hansen"),
+    new Message(2, "Printer broken", "The printer in office 23 is scheduled for repairs. Please use the printer in office 22.", "John Matus"),
+    new Message(3, "Printer back online", "The printer in office 23 has been repaired. Regular print jobs may resume.", "Lacey Wilcox")
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
 
 }
