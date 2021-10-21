9ef30127ec95ef95804c0a141fbd5e9235a5546f
Breanna Hansen <hansbrea@hotmail.com>
2021-10-21 12:00:12 -0600

    Define routerlink for DocumentDetailComponent

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -15,9 +15,5 @@ export class DocumentListComponent implements OnInit {
   ngOnInit(): void {
     this.documents = this.documentService.getDocuments();
   }
-
-  onSelectedDocument(document: Document) {
-    this.documentService.documentSelectedEvent.emit(document);
-  }
 }
 
