baa09ee1e56138ee8103117fcc8d826bdd0d2a95
Breanna Hansen <hansbrea@hotmail.com>
2021-10-21 11:22:19 -0600

    Define [routerLink] Directives for Each Route

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+
 import { ContactsComponent } from "./contacts/contacts.component";
 import { DocumentsComponent } from "./documents/documents.component";
 import { MessageListComponent } from "./messages/message-list/message-list.component";
