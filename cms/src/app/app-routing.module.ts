57fc2c400522f54c27f43379aa2099534f1478bc
Breanna Hansen <hansbrea@hotmail.com>
2021-10-21 12:49:21 -0600

    Define routerLink for Contact Edit button

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ContactDetailComponent } from "./contacts/contact-detail/contact-detail.component";
+import { ContactEditComponent } from "./contacts/contact-edit/contact-edit.component";
 
 import { ContactsComponent } from "./contacts/contacts.component";
 import { DocumentDetailComponent } from "./documents/document-detail/document-detail.component";
@@ -18,9 +19,9 @@ const appRoutes: Routes = [
     ]},
     { path: 'messages', component: MessageListComponent },
     { path: 'contacts', component: ContactsComponent, children: [
-        // { path: 'new', component: ContactEditComponent},
+        { path: 'new', component: ContactEditComponent},
         { path: ':id', component: ContactDetailComponent},
-        // { path: ':id/edit', component: ContactEditComponent}
+        { path: ':id/edit', component: ContactEditComponent}
     ]},
 ];
 
