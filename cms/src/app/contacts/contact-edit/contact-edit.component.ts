f25a2e4912d3f811ccd12f8274627db4397cf75b
Breanna Hansen <hansbrea@hotmail.com>
2021-11-03 14:49:25 -0600

    Methods to Add and Delete Contacts

--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -55,14 +55,46 @@ export class ContactEditComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    const value = form.value; 
-    const newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, value.group); 
-    if (this.editMode) { 
+    const value = form.value;
+    const newContact = new Contact(value.id, value.name, value.email, value.phone, value.imageUrl, value.group);
+    if (this.editMode) {
       this.contactService.updateContact(this.originalContact, newContact);
     } else {
-      this.contactService.addContact(newContact);  
-    } 
+      this.contactService.addContact(newContact);
+    }
     this.editMode = false;
-    this.router.navigate(['/contacts']), { relativeTo: this.route }; 
+    this.router.navigate(['/contacts']), { relativeTo: this.route };
+  }
+
+
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {// newContact has no value
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
   }
 }
