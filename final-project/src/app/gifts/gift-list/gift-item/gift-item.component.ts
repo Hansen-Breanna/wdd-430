53f7ca48c2637ef9a783a2f22b57b8785738467e
Breanna Hansen <hansbrea@hotmail.com>
2021-12-02 23:11:05 -0700

    remove placeholder text

--- final-project/src/app/gifts/gift-list/gift-item/gift-item.component.ts
+++ final-project/src/app/gifts/gift-list/gift-item/gift-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Gift } from '../../gift.model';
 
 @Component({
   selector: 'app-gift-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gift-item.component.css']
 })
 export class GiftItemComponent implements OnInit {
+  @Input() gift: Gift;
+  @Input() index: number;
 
   constructor() { }
 
